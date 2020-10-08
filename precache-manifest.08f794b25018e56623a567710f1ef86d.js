0598951229c9ee0fde214b4854ce3a9ead7b587c
Salvador Ahumada <ahumada1790@gmail.com>
2020-10-07 22:40:19 -0500

    Updates

---
+++ precache-manifest.08f794b25018e56623a567710f1ef86d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e6408cde5a34f9c8c4084fd92f7b31",
+    "url": "/inbersus-app/index.html"
+  },
+  {
+    "revision": "c85edd56271dbad49c4f",
+    "url": "/inbersus-app/static/css/main.5d3b8cd6.chunk.css"
+  },
+  {
+    "revision": "11709ce9131c203f1eda",
+    "url": "/inbersus-app/static/js/2.0d0cdb78.chunk.js"
+  },
+  {
+    "revision": "c85edd56271dbad49c4f",
+    "url": "/inbersus-app/static/js/main.98817c1f.chunk.js"
+  },
+  {
+    "revision": "aea98672f424c896cdf1",
+    "url": "/inbersus-app/static/js/runtime-main.b27f1e7f.js"
+  },
+  {
+    "revision": "686f65094756f21c62d0636f0c2acf07",
+    "url": "/inbersus-app/static/media/logo.686f6509.svg"
+  }
+]);
